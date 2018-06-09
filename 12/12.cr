ffcbb7bba2580390736e538499a78fed42094019
Ben Haney <ben@benhaney.com>
2018-06-09 14:12:11 -0500

    Man crystal is neat

--- 12/12.cr
+++ 12/12.cr
@@ -1,14 +1,15 @@
 def divisors(x)
   divs = 0
-  y = Math.sqrt(x).to_i32
+  y = Math.sqrt(x)
   (1..y).each do |i|
-    divs += 2 if (x % i) == 0
+    divs += 2 if x % i == 0
   end
-  return divs + 1;
+  return divs + 1
 end
 
 tri, i = 0, 0
 until divisors(tri) > 500
   tri += (i += 1)
 end
+
 puts tri
